3ea727a2deb75a37a04e1fead42a14d4837f7849
654Sans456 <87007646+654Sans456@users.noreply.github.com>
2025-03-01 23:07:56 +0300

    Initial commit

---
+++ ElectronicSchoolDaybook/LoginWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace ElectronicSchoolDaybook
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+
+            var isUsername = DB.Context.Users.FirstOrDefault(u => (u.Username == username));
+            var isPassword = DB.Context.Users.FirstOrDefault(u => (u.Password == password));
+
+            if ((isUsername != null) && (isPassword != null))
+            {
+                Session.CurrentUser = DB.Context.Users.FirstOrDefault(u => ((u.Username == username) && (u.Password == password)));
+
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+
+                this.Close();
+            }
+            else if (isUsername == null)
+            {
+                MessageBox.Show("Неверный логин!");
+            }
+            else if (isPassword == null)
+            {
+                MessageBox.Show("Неверный пароль!");
+            }
+        }
+    }
+}
---
+++ ElectronicSchoolDaybook/Session.cs
@@ -0,0 +1,7 @@
+﻿namespace ElectronicSchoolDaybook
+{
+    public static class Session
+    {
+        public static User CurrentUser { get; set; }
+    }
+}
